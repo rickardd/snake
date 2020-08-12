4a24b950a4370c440a14a959423d0a3bdfe8e2fe
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2020-08-13 11:48:21 +1200

    SW: Changed cache version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var version = 'v1::';
+var version = 'v2::';
 
 self.addEventListener("install", function (event) {
     console.log('WORKER: install event in progress.');
@@ -33,34 +33,12 @@ self.addEventListener("fetch", function (event) {
                 var networked = fetch(event.request)
                     .then(fetchedFromNetwork, unableToResolve)
                     .catch(unableToResolve);
+
+                // This will return cached version if it exist, else network version.
+                // Might prevent installed apps to not be updated when changes has been made. 
                 console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+
                 return cached || networked;
-
-                function fetchedFromNetwork(response) {
-                    var cacheCopy = response.clone();
-                    console.log('WORKER: fetch response from network.', event.request.url);
-                    caches
-                        .open(version + 'pages')
-                        .then(function add(cache) {
-                            cache.put(event.request, cacheCopy);
-                        })
-                        .then(function () {
-                            console.log('WORKER: fetch response stored in cache.', event.request.url);
-                        });
-
-                    return response;
-                }
-
-                function unableToResolve() {
-                    console.log('WORKER: fetch request failed in both cache and network.');
-                    return new Response('<h1>Service Unavailable</h1>', {
-                        status: 503,
-                        statusText: 'Service Unavailable',
-                        headers: new Headers({
-                            'Content-Type': 'text/html'
-                        })
-                    });
-                }
             })
     );
 });
@@ -86,3 +64,30 @@ self.addEventListener("activate", function (event) {
             })
     );
 });
+
+
+function fetchedFromNetwork(response) {
+    var cacheCopy = response.clone();
+    console.log('WORKER: fetch response from network.', event.request.url);
+    caches
+        .open(version + 'pages')
+        .then(function add(cache) {
+            cache.put(event.request, cacheCopy);
+        })
+        .then(function () {
+            console.log('WORKER: fetch response stored in cache.', event.request.url);
+        });
+
+    return response;
+}
+
+function unableToResolve() {
+    console.log('WORKER: fetch request failed in both cache and network.');
+    return new Response('<h1>Service Unavailable</h1>', {
+        status: 503,
+        statusText: 'Service Unavailable',
+        headers: new Headers({
+            'Content-Type': 'text/html'
+        })
+    });
+}
